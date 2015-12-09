b199bd1aec9835c34274839ea16a4f35cda7bdb4
Mike Wickwar <mike.wickwar@gmail.com>
2015-12-09 15:38:15 -0700

    pages are linked together mod 1 complete

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  console.log("hello world");
+var btn1 = document.getElementById("cTBtn");
+
+btn1.addEventListener('click',function(){
+  window.location='index2.html';
+})
+
+})
