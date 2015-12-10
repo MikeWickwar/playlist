769cb738c131eab7caf9004f104f3818c54a00d7
Mike Wickwar <mike.wickwar@gmail.com>
2015-12-10 08:16:34 -0700

    mod5 complete time to refactor

--- dom2.js
+++ dom2.js
@@ -17,7 +17,7 @@ getter.done(function(response){
   console.log(response);
   a = response;
   for (var i = 0; i < response.results.length; i++) {
-    console.log(getImgs(response, i));
+  console.log(getImgs(response, i));
       $("photos").append("<img id='"+[i]+"' class='sImg' src='images/"+getImgs(response, i)+"' alt='' />");
       }
 
@@ -28,17 +28,26 @@ pGetter.done(function(response){
   var a = response;
 })
 
-
 $(document).on('click','.sImg',function(response){
-  console.log("hello");
   var i = parseInt(this.id);
-  console.log(i);
-  console.log(a);
   var acu = $('textarea').val();
   console.log(acu);
   acu += $('textarea').val(a.results[i].title +"\n"+acu);
   console.log(a.results[i].title);
 })
+$(document).on('click','#cTBtn2a',function(){
+  $('textarea').val('');
+})
+$(document).on('click','#cTBtn2b',function(){
+  var out = $('textarea').val();
+  console.log(out);
 
-//start with this piece of awesome. just need to
-//get the Accumulator working
+  var pGetter = $.ajax({
+    url: "https://lit-fortress-6467.herokuapp.com/post",
+    method: "POST"
+  });
+  pGetter.done(function(response){
+    console.log(response);
+    var a = response;
+  })
+})
